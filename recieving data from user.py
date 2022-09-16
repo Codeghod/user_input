5e0b8ac39793dd55569c1f45cac6ece1df731527
Alfred Turkson <111619866+Codeghod@users.noreply.github.com>
2022-09-15 17:01:59 -0700

    Add files via upload

---
+++ recieving data from user.py
@@ -0,0 +1,7 @@
+# How to accept data from the user in python
+# The 'input' function is used to recieve data from the user
+
+name = input('what is your name? ')
+age = input('what is your age? ')
+favourite_color = input('what is your favourite color? ')
+print('All details submitted successfully')
